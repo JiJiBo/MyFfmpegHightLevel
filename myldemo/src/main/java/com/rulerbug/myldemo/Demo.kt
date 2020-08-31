44da7a2a2c2e6698c7345b205855ddf6fde49839
xieangxuan <767950460@qq.com>
2020-08-31 14:50:19 +0800

    refresh to hight

--- myldemo/src/main/java/com/rulerbug/myldemo/Demo.kt
+++ myldemo/src/main/java/com/rulerbug/myldemo/Demo.kt
@@ -13,15 +13,24 @@ class Demo {
 
             // Used to load the 'native-lib' library on application startup.
 
-            System.loadLibrary("native-lib");
-            System.loadLibrary("avcodec-57");
+//            System.loadLibrary("native-lib");
+//            System.loadLibrary("avcodec-57");
 //            System.loadLibrary("avdevice-57");
 //            System.loadLibrary("avfilter-6");
-            System.loadLibrary("avformat-57");
-            System.loadLibrary("avutil-55");
+//            System.loadLibrary("avformat-57");
+//            System.loadLibrary("avutil-55");
 //            System.loadLibrary("postproc-54");
-            System.loadLibrary("swresample-2");
-            System.loadLibrary("swscale-4");
+//            System.loadLibrary("swresample-2");
+//            System.loadLibrary("swscale-4");
+            System.loadLibrary("native-lib");
+            System.loadLibrary("avcodec");
+            System.loadLibrary("avdevice");
+            System.loadLibrary("avfilter");
+            System.loadLibrary("avformat");
+            System.loadLibrary("avutil");
+            System.loadLibrary("postproc");
+            System.loadLibrary("swresample");
+            System.loadLibrary("swscale");
 
         }
     }
